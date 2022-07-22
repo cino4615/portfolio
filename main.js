cf07596d3467b14d44a1c53bf3bb87c8334e54e4
cino4615 <61144949+cino4615@users.noreply.github.com>
2022-07-22 11:05:16 +0900

    js transparent

--- main.js
+++ main.js
@@ -23,10 +23,17 @@ navbarMenu.addEventListener('click', (event) => {
     scrollIntoView(link);
 });
 
-//Handle click on "contact me" button on home
+// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
+});
+
+// Make home solwly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 function scrollIntoView(selector) {
